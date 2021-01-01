e126b13fc0c85f3dfec55700bce95c774bee7670
nbutluo <40694420+nbutluo@users.noreply.github.com>
2021-01-01 16:26:47 +0800

    话题回复

--- routes/api.php
+++ routes/api.php
@@ -74,6 +74,8 @@ Route::prefix('v1')->namespace('Api')
                     Route::resource('topics', 'TopicsController')->only([
                         'store', 'update', 'destroy'
                     ]);
+                    // 发布回复
+                    Route::post('topics/{topic}/replies', 'RepliesController@store')->name('topics.replies.store');
                 });
             });
     });
